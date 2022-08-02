f0b100e82b06eb399c551301dfc4cd09935debb1
Ada Morgan <54023403+AdaMorgan@users.noreply.github.com>
2022-08-02 12:24:09 +0300

    Commit message

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import './App.css';
+
+const root = ReactDOM.render(
+  <div className="App">
+    <header className="App-header">
+      <p>Edit src/App.js and save to reload.</p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </header>
+  </div>
+, document.getElementById("root"));
